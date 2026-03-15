fa4964d83ea3d8664368f5617025ac858adb2f58
ArchSeraphin <trinity.informatique@gmail.com>
2026-03-15 12:27:19 +0100

    v1.0.5
    Bug fix

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net', 'unpkg.com', 'cdnjs.cloudflare.com', 'fonts.googleapis.com'],
+      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'cdn.jsdelivr.net', 'unpkg.com', 'cdnjs.cloudflare.com', 'fonts.googleapis.com'],
       styleSrc:  ["'self'", "'unsafe-inline'", 'fonts.googleapis.com', 'cdnjs.cloudflare.com'],
       fontSrc:   ["'self'", 'fonts.gstatic.com', 'cdnjs.cloudflare.com'],
       imgSrc:    ["'self'", 'data:', 'blob:', '*'],
